0abdfb0e199890e207208c8df1522ca6fe1ebbe8
[Daniel Liu] <[daniel.liu74@qmail.cuny.edu]>
2025-12-17 16:30:09 -0500

    Added UI automation test for SauceDemo login using Playwright

---
+++ test_ui.py
@@ -0,0 +1,28 @@
+import pytest
+from playwright.sync_api import Page, expect
+
+def test_saucedemo_login(page: Page):
+    
+    """
+    Test case: Verify
+    """
+
+    print("\n[Step] Navigating to the login page...")
+    page.goto("https://www.saucedemo.com/")
+
+    expect(page).to_have_title("Swag Labs")
+
+    print("[Step] Entering username...")
+    page.fill("#user-name", "standard_user")
+
+    print("[Step] Entering password...")
+    page.fill("#password", "secret_sauce")
+
+    print("[Step] Clicking login button...")
+    page.click("#login-button")
+
+    print("[Step] Verifying login success...")
+
+    expect(page.locator(".tittle")).to_have_text("Products")
+
+    expect(page.locator(".shopping_cart_link")).to_be_visible
